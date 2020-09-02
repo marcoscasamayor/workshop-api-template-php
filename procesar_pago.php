ba477930ac1a3267b134404064204cf17079cad3
Marcos C <marcos.casamayor@gmail.com>
2020-09-02 15:50:52 -0300

    ech

--- procesar_pago.php
+++ procesar_pago.php
@@ -3,13 +3,11 @@
 
     MercadoPago\SDK::setAccessToken("TEST-8698162945950227-060506-ae0c2b04a50b5ff6b1dcdb7f8039f10b-53340008");
 
-    echo("vamos 1");
-
     $payment = new MercadoPago\Payment();
     $payment->transaction_amount = (float)$_POST['transactionAmount'];
     $payment->token = $_POST['token'];
     $payment->description = $_POST['description'];
-    $payment->installments = (int)$_POST['installments'];
+    $payment->installments = 1;
     $payment->payment_method_id = $_POST['paymentMethodId'];
     $payment->issuer_id = (int)$_POST['issuer'];
 
